2b6ccdbc4d2b3df2bb24945cfd0417a83f42692e
rampagetrew-ai <rampagetrew@gmail.com>
2026-02-21 10:14:31 +0300

    Add bot/keyboards/__init__.py

---
+++ bot/keyboards/__init__.py
@@ -0,0 +1,15 @@
+"""Клавиатуры бота."""
+
+from bot.keyboards.inline import (
+    back_to_menu_kb,
+    main_menu_kb,
+    numerology_menu_kb,
+    tarot_menu_kb,
+)
+
+__all__ = [
+    "back_to_menu_kb",
+    "main_menu_kb",
+    "numerology_menu_kb",
+    "tarot_menu_kb",
+]
